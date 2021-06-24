757a127fd6adb7e26811e90d697cc10b3eb90970
Guo-zy <674402145@qq.com>
2021-06-24 21:20:50 +0800

    add accout call withdraw junit test

--- src/test/java/com/tw/banking/AccountTest.java
+++ src/test/java/com/tw/banking/AccountTest.java
@@ -26,4 +26,24 @@ class AccountTest {
         );
     }
 
+    @Test
+    void should_transactionRepository_call_withdraw_succussful_and_use_correct_amount_when_account_call_withdraw_given_amount(){
+        //give
+        TransactionRepository transactionRepository = mock(TransactionRepository.class);
+        ArgumentCaptor<Integer> amountCaptor = ArgumentCaptor.forClass(Integer.class);
+        Printer printer = new Printer(new Console());
+        Account account = new Account(transactionRepository,printer);
+        final Integer amount = 100;
+
+        //when
+        account.withdraw(amount);
+
+        //then
+        assertAll(
+                ()->verify(transactionRepository,times(1)).addWithdraw(amountCaptor.capture()),
+                ()->assertEquals(amount, amountCaptor.getValue())
+        );
+    }
+
+
 }
